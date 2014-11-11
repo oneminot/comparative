ff9aef1f406d918046c267bf257a82b0f1d6bd0d
kushal <kushaldeveloper@gmail.com>
2014-11-11 17:36:42 -0600

    builds just fine

--- Comparative/main.cpp
+++ Comparative/main.cpp
@@ -1,25 +1,99 @@
-/* rand example: guess the number */
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-	int iSecret, iGuess;
-
-	/* initialize random seed: */
+	int iSecret;
 	srand(time(NULL));
-
-	/* generate secret number between 1 and 10: */
 	iSecret = rand() % 10 + 1;
+	if (iSecret % 2 == 0)
+	{
+		if (iSecret % 2 == 0)
+		{
+			if (iSecret % 2 == 0)
+			{
+				if (iSecret % 2 == 0)
+				{
+					if (iSecret % 2 == 0)
+					{
+						if (iSecret % 2 == 0)
+						{
+							if (iSecret % 2 == 0)
+							{
+								if (iSecret % 2 == 0)
+								{
+									if (iSecret % 2 == 0)
+									{
+										if (iSecret % 2 == 0)
+										{
+											if (iSecret % 2 == 0)
+											{
+												if (iSecret % 2 == 0)
+												{
+													if (iSecret % 2 == 0)
+													{
+														if (iSecret % 2 == 0)
+														{
+															if (iSecret % 2 == 0)
+															{
+																if (iSecret % 2 == 0)
+																{
+																	if (iSecret % 2 == 0)
+																	{
+																		if (iSecret % 2 == 0)
+																		{
+																			if (iSecret % 2 == 0)
+																			{
+																				if (iSecret % 2 == 0)
+																				{
+																					if (iSecret % 2 == 0)
+																					{
+																						if (iSecret % 2 == 0)
+																						{
+																							if (iSecret % 2 == 0)
+																							{
+																								if (iSecret % 2 == 0)
+																								{
+																									if (iSecret % 2 == 0)
+																									{
+																										if (iSecret % 2 == 0)
+																										{
+																											if (iSecret % 2 == 0)
+																											{
+																												if (iSecret % 2 == 0)
+																												{
+																													if (iSecret % 2 == 0)
+																													{
 
-	do {
-		printf("Guess the number (1 to 10): ");
-		scanf("%d", &iGuess);
-		if (iSecret<iGuess) puts("The secret number is lower");
-		else if (iSecret>iGuess) puts("The secret number is higher");
-	} while (iSecret != iGuess);
-
-	puts("Congratulations!");
+																													}
+																												}
+																											}
+																										}
+																									}
+																								}
+																							}
+																						}
+																					}
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 	return 0;
 }
